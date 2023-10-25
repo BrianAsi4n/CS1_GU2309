2ac63477358e2598edeecd5f68616e33bd849d9d
BrianAsi4n <146366610+BrianAsi4n@users.noreply.github.com>
2023-10-25 08:26:57 +0700

    btvn
    btvn

--- B11/CGO11/CGO11/Program.cs
+++ B11/CGO11/CGO11/Program.cs
@@ -10,6 +10,23 @@ namespace CGO11
     {
         static void Main(string[] args)
         {
+            int[] arr1D = { 2, 3, 4, 5, 6, 7, 8};
+            int[,] arr2D = new int[3, 3];
+
+            for (int i = 0; i < arr1D.Length; i++)
+            {
+                arr2D[i / 3, i % 3] = arr1D[i];
+            }
+
+            Console.WriteLine("Ma tran vua chuyen doi la:");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(arr2D[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ B11/btvn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btvn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3, m = 3;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[,] matrix = new int[n, m];
+
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = arr[k];
+                    k++;
+                }
+            }
+
+            Console.WriteLine("Ma tran vua nhap la:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
